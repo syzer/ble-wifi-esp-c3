1c3472ebd7816325cdf057f3a8225f6738bd719b
Lukasz Gintowt <syzer3@gmail.com>
2025-06-13 16:36:30 +0200

    wip

--- readme.md
+++ readme.md
@@ -3,6 +3,6 @@ Adds WiFi RTT capability to all clients,
 forwards internet via NAT
 
 
-```
+```bash
 cargo run
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,49 @@
-use anyhow::Result;
-use anyhow::anyhow;
+#![no_main]
+#![feature(impl_trait_in_assoc_type)]
+
+use esp_println::println;  
+use anyhow::{Result, anyhow};
+
 use esp_idf_svc::{
     hal::{
         gpio::{InterruptType, PinDriver, Pull},
         peripherals::Peripherals,
         task::notification::Notification,
     },
-    sys::esp_random,
+    // sys::esp_random,
 };
 
 mod rgb_driver;
 use rgb_driver::{RGB8, WS2812RMT};
-use std::num::NonZeroU32;
-use getrandom::getrandom;
-// use esp_idf_svc::hal::rmt::WS2812RMT;
+// use std::num::NonZeroU32;
+// use num_traits::NonZeroU32;
+use core::num::NonZeroU32;
 
-fn main() -> Result<()> {
+use getrandom::getrandom;
+use embassy_executor::Spawner;
+
+// use esp_idf_svc::hal::rmt::WS2812RMT;
+// use esp_hal::{clock::ClockControl, peripherals::Peripherals, system::SystemControl};
+
+
+// #[panic_handler]
+// fn panic(_: &core::panic::PanicInfo) -> ! {
+//     println!("No panic 🐕, just a loop!");
+//     loop {};
+// }
+
+// #[embassy_executor::main]
+// #[esp_hal_embassy::main]
+
+
+#[esp_hal::main]
+async fn main(_spawner: Spawner) -> anyhow::Result<()> {
+
+// async fn main(_spawner: embassy_executor::Spawner) -> Result<()> {
     esp_idf_svc::sys::link_patches();
 
     let peripherals = Peripherals::take()?;
-    // ANCHOR: led
     let mut led = WS2812RMT::new(peripherals.pins.gpio2, peripherals.rmt.channel0)?;
-    // ANCHOR_END: led
 
     // Configures the button
     let mut button = PinDriver::input(peripherals.pins.gpio9)?;
@@ -40,7 +62,6 @@ fn main() -> Result<()> {
         })?;
     }
 
-    // ANCHOR: loop
     loop {
         // Enable interrupt and wait for new notificaton
         button.enable_interrupt()?;
@@ -49,7 +70,6 @@ fn main() -> Result<()> {
         // Generates random rgb values and sets them in the led.
         let _ = random_light(&mut led);
     }
-    // ANCHOR_END: loop
 }
 
 #[allow(unused)]
@@ -60,9 +80,14 @@ pub fn random_light(led: &mut WS2812RMT) -> Result<()> {
         return Err(anyhow!("RNG failed: {:?}", e));
     }
 
-    // let colour = RGB8::new(buf[0], buf[1], buf[2]);
+    let brightness = 8; // Adjust brightness level (1-255)
+
      // Reduce brightness by dividing by 8 (or adjust divisor for different brightness levels)
-    let colour = RGB8::new(buf[0] / 8, buf[1] / 8, buf[2] / 8);
+    let colour = RGB8::new(
+        (buf[0] as u32 * brightness / 255) as u8,
+        (buf[1] as u32 * brightness / 255) as u8,
+        (buf[2] as u32 * brightness / 255) as u8,
+    );
 
 
     // Push colour to the LED
